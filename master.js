a464428cbe22c396083e3451c056d04a7b2e841d
Jesus Gallego Olivas <jesus.gallego.o@gmail.com>
2017-03-01 17:23:33 +0100

    better03

--- master.js
+++ master.js
@@ -1,21 +1,19 @@
 var screen = document.getElementById('display');
 
-function adder (optag, restag) {
-  var operation = document.getElementById(optag);
-  var operands = operation.innerHTML.split ("+");
-  var result = document.getElementById(restag);
+function enter (display, resultado) {
+  var display = document.getElementById(display);
+  var operands = display.innerHTML.split ("+");
+  var resultado = document.getElementById(resultado);
   var resultData = parseInt(operands[0]) ^ parseInt(operands[1]);
 
-  result.innerHTML = "= " + resultData;
+  resultado.innerHTML = resultData;
 }
 function key(number){
-  //alert(number);
   screen.innerHTML=screen.innerHTML+number;
 }
 
 function del(){
   var display = document.getElementById("display");
-
   var borrado=display.innerHTML.slice(0,-1);
   display.innerHTML=borrado;
 }
